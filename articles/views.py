d18ac257f711c96a52a5904ce583ddaae5fa383f
leeyounghuncom <dosiraklab@gmail.com>
2024-08-10 17:57:56 +0900

    HTML Form

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,42 @@
+from urllib import request
+
 from django.shortcuts import render
+
+
 # from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
-	# response = HttpResponse("<h1>Hello, Django!</h1>")
-	# return response
+
+
+# response = HttpResponse("<h1>Hello, Django!</h1>")
+# return response
 
 def users(request):
-        return render(request, "users.html")
+    return render(request, "users.html")
+
+
+def hello(request):
+    name = "희경"
+    tags = ["파이썬", "장고", "html", "css", "js"]
+    books = ["한글", "영어"]
+    context = {
+        "name": name,
+        "tags": tags,
+        "books": books
+    }
+    return render(request, "hello.html", context)
+
+
+def data_throw(request):
+    return render(request, "data_throw.html")
+
+
+def data_catch(request):
+    message = request.GET.get("message")
+    context = {
+        "data": message,
+    }
+    return render(request, "data_catch.html", context)
+# return render(request, "data_catch.html")
