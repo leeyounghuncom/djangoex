f26bfd1c195936244f4f89593fb9b9d6f012dd5e
leeyounghuncom <web3dao@kakao.com>
2024-08-11 11:46:17 +0900

    Django ORM

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name="hello"),
+    path("", views.articles, name="articles"),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
+    # path("hello/", views.hello, name="hello"),
     path("data-throw/", views.data_throw, name="throw"),
     path("data-catch/", views.data_catch, name="catch"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from urllib import request
-
 from django.shortcuts import render
+from .models import Article
 
 
 # from django.http import HttpResponse
@@ -13,16 +13,17 @@ def index(request):
 # response = HttpResponse("<h1>Hello, Django!</h1>")
 # return response
 
-def hello(request):
-    name = "희경"
-    tags = ["파이썬", "장고", "html", "css", "js"]
-    books = ["한글", "영어"]
-    context = {
-        "name": name,
-        "tags": tags,
-        "books": books
-    }
-    return render(request, "hello.html", context)
+# def hello(request):
+#     name = "희경"
+#     tags = ["파이썬", "장고", "html", "css", "js"]
+#     books = ["한글", "영어"]
+#     context = {
+#         "name": name,
+#         "tags": tags,
+#         "books": books
+#     }
+#     return render(request, "hello.html", context)
+# heloo.html_ change
 
 
 def data_throw(request):
@@ -35,4 +36,30 @@ def data_catch(request):
         "data": message,
     }
     return render(request, "data_catch.html", context)
+
+
 # return render(request, "data_catch.html")
+def articles(request):
+    articles = Article.objects.all()
+    context = {
+        "articles": articles,
+    }
+    return render(request, "articles.html", context)
+
+
+def new(request):
+    return render(request, "new.html")
+
+
+def create(request):
+    # POST 방식으로 전달된 데이터를 받아서
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+
+    # 받은 데이터를 Article 모델을 이용해서 저장
+    article = Article(title=title, content=content)
+    article.save()
+    context = {
+        "article": article,
+    }
+    return render(request, "create.html", context)
